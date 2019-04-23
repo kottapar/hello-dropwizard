3af098d7c929e7db2eb3e1645dfff884720ecc63
Ravi <kottapar@gmail.com>
2019-04-24 00:15:50 +0400

    adding vagrants and ansible

---
+++ setupdev-windows.bat
@@ -0,0 +1,4 @@
+:: Batch script to spin up the vagrant dev env
+
+SET VAGRANT_CWD=./vagrants
+vagrant up --provider=virtualbox
---
+++ setupdev.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+########################################
+#
+# Script to spin up the vagrant dev env
+#
+########################################
+
+clear
+export VAGRANT_CWD=./vagrants
+vagrant up --provider=virtualbox
